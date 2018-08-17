6bb20c5e7fa33d8bad295de0681ee0fdadaa22ae
Flinn Burgess <flinn.burgess@gmail.com>
2018-08-17 16:49:05 +0100

    Added test for book equality method

---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+public class BookTest {
+
+    @Test
+    public void testBookEquality() {
+        Book book1 = new Book("title", "author", 2008);
+        Book book2 = new Book("title", "author", 2008);
+
+        assert (book1.equals(book2));
+    }
+}
