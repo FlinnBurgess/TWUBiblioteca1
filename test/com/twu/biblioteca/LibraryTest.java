2d5e0e0826fb496b61300fec9f04f726fd56f02e
Flinn Burgess <flinn.burgess@gmail.com>
2018-08-18 12:13:52 +0100

    Added assertion to test case to compare differing booklists

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -12,10 +12,13 @@ public class LibraryTest {
     public void testLibraryReturnsCorrectBookList() {
         Book book1 = new Book("title1", "author1", 2000);
         Book book2 = new Book("title2", "author2", 2000);
+
         ArrayList<Book> bookList = new ArrayList<Book>(Arrays.asList(book1, book2));
+        ArrayList<Book> differentBookList = new ArrayList<Book>(Arrays.asList(book1));
 
         Library library = new Library(bookList);
 
-        Assert(bookList.equals(library.getBooks()));
+        assert (bookList.equals(library.getBooks()));
+        assert (!differentBookList.equals(library.getBooks()));
     }
 }
