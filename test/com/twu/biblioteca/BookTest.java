209812414654f7ec17b8a20cce61a374147b4b66
Flinn Burgess <flinn.burgess@gmail.com>
2018-08-19 11:40:15 +0100

    Updated BookTest to test that equality is case insensitive

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -6,7 +6,7 @@ public class BookTest {
 
     @Test
     public void testBookEquality() {
-        Book book1 = new Book("title", "author", 2008);
+        Book book1 = new Book("TITLE", "author", 2008);
         Book book2 = new Book("title", "author", 2008);
 
         assert (book1.equals(book2));
