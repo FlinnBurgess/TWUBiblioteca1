b96ba6e93963e03be4fa32dcfd26d6bb42f6b089
flinnburgess <flinn.burgess@gmail.com>
2018-09-14 15:14:13 +0100

    Added tests for Movie getter methods and refactored tests to use fewer magic numbers

--- test/com/twu/biblioteca/MovieTest.java
+++ test/com/twu/biblioteca/MovieTest.java
@@ -1,19 +1,50 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
 public class MovieTest {
+    private String testName = "name";
+    private int testYear = 2005;
+    private String testDirector = "director";
+    private int testRating = 5;
 
     @Test
     public void testMovieEquality() {
-        Movie movie = new Movie("name", 2005, "director", 5);
-        Movie sameMovie = new Movie("NAME", 2005, "director", 5);
+        Movie movie = createRatedMovie();
+        Movie sameMovie = createRatedMovie();
 
         Assert.assertTrue(movie.equals(sameMovie));
 
-        Movie differentMovie = new Movie("different name", 2005, "director", 5);
+        Movie differentMovie = new Movie("different name", testYear, testDirector, testRating);
 
         Assert.assertFalse(movie.equals(differentMovie));
+    }
+
+    @Test
+    public void testHasRatingReturnsCorrectValues() {
+        Movie rated = createRatedMovie();
+        Assert.assertTrue(rated.hasRating());
+
+        Movie unrated = createUnratedMovie();
+        Assert.assertFalse(unrated.hasRating());
+    }
+
+    @Test
+    public void testGetRatingReturnsCorrectValue() {
+        Movie rated = createRatedMovie();
+        Assert.assertTrue(rated.getRating() == testRating);
+
+        Movie unrated = createUnratedMovie();
+        Assert.assertTrue(unrated.getRating() == -1);
+    }
+
+    private Movie createRatedMovie() {
+        return new Movie(testName, testYear, testDirector, testRating);
+    }
+
+    private Movie createUnratedMovie() {
+        return new Movie(testName, testYear, testDirector);
     }
 }
