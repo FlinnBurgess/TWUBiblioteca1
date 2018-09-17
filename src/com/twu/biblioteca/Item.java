8891b308c641b94f6b25d8bdbfe497e7996666ad
flinnburgess <flinn.burgess@gmail.com>
2018-09-17 11:49:12 +0100

    Added item superclass

---
+++ src/com/twu/biblioteca/Item.java
@@ -0,0 +1,45 @@
+package com.twu.biblioteca;
+
+public class Item implements Comparable<Item> {
+    String name;
+    String creator;
+    int year;
+
+    public Item(String name, String creator, int year) {
+        this.name = name;
+        this.creator = creator;
+        this.year = year;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getCreator() {
+        return creator;
+    }
+
+    int getYear() {
+        return year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+
+        Item item = (Item) o;
+
+        return (
+            name.toLowerCase().equals(item.getName().toLowerCase())
+            && year == item.getYear()
+            && creator.toLowerCase().equals(item.getCreator().toLowerCase())
+        );
+    }
+
+    @Override
+    public int compareTo(Item item) {
+        return this.name.compareTo(item.getName());
+    }
+}
